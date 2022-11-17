8668e9134b152db247fbe0fdfa48b39ac0a33adc
lee-emqx <lihj@emqx.io>
2022-11-17 11:54:07 +0800

    * UPDATE [test] update test.c.

--- test.c
+++ test.c
@@ -6,12 +6,18 @@ int main(int argc, char *argv[])
 {
     if (argc > 1) {
         cJSON *ret = hocon_parse_file(argv[1]);
-        puts(cJSON_PrintUnformatted(ret));
+        char *str = cJSON_PrintUnformatted(ret);
+        puts(str);
+        cJSON_free(str);
+        cJSON_Delete(ret);
+
+    } else {
+        fprintf(stderr, "Usage: test <your conf file>\n");
     }
 
 
-    char str[] = "abc=1";
-    hocon_parse_str(str, strlen(str));
+    // char str[] = "abc=1";
+    // hocon_parse_str(str, strlen(str));
     return 0;
 
 }
